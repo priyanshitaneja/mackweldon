4c925b32c5bdf09299f18654ccd29330988c8848
priyanshitaneja <priyanshi.taneja@oriserve.com>
2022-06-10 14:10:14 +0530

    desktop header created + styled

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,26 @@
-import '../styles/index.scss';
+import '../data/styles/index.scss';
+import '../data/styles/header.scss';
 
 console.log('webpack starterkit');
+
+// var headerPromo = document.getElementsByClassName('promo');
+// var stuck = false;
+// var stickPoint = getDistance();
+
+// function getDistance() {
+//   var topDist = headerPromo.offsetTop;
+//   return topDist;
+// }
+
+// window.onscroll = function () {
+//   var distance = getDistance() - window.pageYOffset;
+//   var offset = window.pageYOffset;
+//   if (distance <= 0 && !stuck) {
+//     headerPromo.style.position = 'fixed';
+//     headerPromo.style.top = '0px';
+//     stuck = true;
+//   } else if (stuck && offset <= stickPoint) {
+//     headerPromo.style.position = 'static';
+//     stuck = false;
+//   }
+// };

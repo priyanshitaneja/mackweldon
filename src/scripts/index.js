d111010898375cf6a8321ebcf7778317a8fb2058
priyanshitaneja <priyanshi.taneja@oriserve.com>
2022-06-14 02:36:36 +0530

    added password

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,16 @@ import '../data/styles/doubleImgDiv.scss';
 
 import './header.js';
 import './section1.js';
+
+(function() {
+  var password = prompt('Password Please');
+  if (password != 'Please') {
+    document.body.innerHTML = '';
+    document.body.innerHTML =
+      'Uh-oh! Incorrect password <br/> <br/> Try again?';
+    document.body.style.textAlign = "center";
+    document.body.style.marginTop = "100px";
+  } else {
+    alert("Come right in \nYou've entered the right password 🎉 🎉 🎉");
+  }
+})();

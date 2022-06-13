455cf46dbb9794c2cd2ebdf44c5a9acdc7dcf176
priyanshitaneja <priyanshi.taneja@oriserve.com>
2022-06-14 01:05:46 +0530

    section 1 mobile view

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import '../data/styles/index.scss';
 import '../data/styles/common.scss';
 
 import '../data/styles/header.scss';
-import '../data/styles/section1.scss';
+import '../data/styles/doubleImgDiv.scss';
 
 import './header.js';
 import './section1.js';

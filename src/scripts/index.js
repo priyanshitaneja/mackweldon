49b8076d4795ec46c6c7ff72e5318ef0b2030e61
priyanshitaneja <priyanshi.taneja@oriserve.com>
2022-06-14 00:49:17 +0530

    section 1 desktop view

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 import '../data/styles/index.scss';
-import '../data/styles/header.scss';
 import '../data/styles/common.scss';
 
+import '../data/styles/header.scss';
+import '../data/styles/section1.scss';
+
 import './header.js';
+import './section1.js';
---
+++ src/scripts/section1.js
@@ -0,0 +1,18 @@
+// const imgCTAs = document.querySelectorAll('.img-cta');
+// const arrowRight = document.querySelectorAll('.img-cta i');
+
+// imgCTAs.forEach((cta) =>
+//   cta.addEventListener('mouseover', () => {
+//     console.log("cta", cta);
+//     arrowRight.classList.add('pad-l-12');
+//   }),
+// );
+
+// const imgCTAs = document.querySelector('.img-cta');
+// const arrowRight = document.querySelector('.img-cta i');
+
+// imgCTAs.addEventListener('mouseover', () => {
+//     console.log('cta', imgCTAs);
+//     arrowRight.classList.add('pad-l-12');
+//     console.log("arrowRRRR classes", arrowRight.classList);
+// });

471c0ae1fce790828773bd7cf3786fd5a35bc894
priyanshitaneja <priyanshi.taneja@oriserve.com>
2022-06-14 17:47:03 +0530

    img cards created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,20 +4,21 @@ import '../data/styles/common.scss';
 import '../data/styles/header.scss';
 import '../data/styles/doubleImgDiv.scss';
 import '../data/styles/section1.scss';
+import '../data/styles/section2.scss';
 import '../data/styles/section3.scss';
 
 import './header.js';
 import './section1.js';
 
-(function() {
-  var password = prompt('Password Please');
-  if (password != 'Please') {
-    document.body.innerHTML = '';
-    document.body.innerHTML =
-      'Uh-oh! Incorrect password <br/> <br/> Try again?';
-    document.body.style.textAlign = "center";
-    document.body.style.marginTop = "100px";
-  } else {
-    alert("Come right in \nYou've entered the right password 🎉 🎉 🎉");
-  }
-})();
+// (function() {
+//   var password = prompt('Password Please');
+//   if (password != 'Please') {
+//     document.body.innerHTML = '';
+//     document.body.innerHTML =
+//       'Uh-oh! Incorrect password <br/> <br/> Try again?';
+//     document.body.style.textAlign = "center";
+//     document.body.style.marginTop = "100px";
+//   } else {
+//     alert("Come right in \nYou've entered the right password 🎉 🎉 🎉");
+//   }
+// })();
